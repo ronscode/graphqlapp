452732a0ac65a4247342febdea7ed181a0752d2d
Ron Cagle <ron@atlantashirt.com>
2019-04-23 23:54:38 -0400

    Good place to save progress

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
+// empty array to be a temp db
+const events = [];
+
 app.use(bodyParser.json());
 
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery {
+        events: [Event!]!
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    // Resolver
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString,
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+
+// Route to make sure the app is on
 app.get("/", (req, res, next) => {
   res.send("Hello SPACE!");
 });
